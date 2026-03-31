736210b9c23d98073da78ec52fc9f8c53bb8d884
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-03-31 15:07:26 -0300

    feat: adicionar arquivos

---
+++ api/disciplinas.js
@@ -0,0 +1,85 @@
+'use strict'
+
+export const DISCIPLINAS = [
+    {
+        "id": 1,
+        "disciplina": "Cargos, Carreira e Remuneração",
+        "sigla": "CCR",
+        "mentor": false
+    },
+    {
+        "id": 2,
+        "disciplina": "Comportamento Organizacional",
+        "sigla": "CO",
+        "mentor": false
+    },
+    {
+        "id": 3,
+        "disciplina": "Comunicação Corporativa",
+        "sigla": "CC",
+        "mentor": false
+    },
+    {
+        "id": 4,
+        "disciplina": "Ética e Cidadania Organizacional",
+        "sigla": "ECO",
+        "mentor": "Marcos Batista dos Santos"
+    },
+    {
+        "id": 5,
+        "disciplina": "Gestão de Desempenho",
+        "sigla": "GD",
+        "mentor": false
+    },
+    {
+        "id": 6,
+        "disciplina": "Língua Espanhola",
+        "sigla": false,
+        "mentor": "Valdecir David da Silva"
+    },
+    {
+        "id": 7,
+        "disciplina": "Língua Inglesa",
+        "sigla": false,
+        "mentor": false
+    },
+    {
+        "id": 8,
+        "disciplina": "Língua Portuguesa",
+        "sigla": false,
+        "mentor": "Abel José Mendes"
+    },
+    {
+        "id": 9,
+        "disciplina": "Matemática",
+        "sigla": false,
+        "mentor": "Eduardo Rodrigo Vedolin"
+    },
+    {
+        "id": 10,
+        "disciplina": "Modelos de Gestão",
+        "sigla": "MG",
+        "mentor": false
+    },
+    {
+        "id": 11,
+        "disciplina": "Planejamento e Desenvolvimento do Trabalho de Conclusão de Curso em Recursos Humanos",
+        "sigla": "TCC",
+        "mentor": [
+            "Adonias de Carvalho",
+            "Ruth de Jesus Macedo"
+        ]
+    },
+    {
+        "id": 12,
+        "disciplina": "Qualidade de Vida, Saúde e Segurança do Trabalho",
+        "sigla": "QVSST",
+        "mentor": "Luiz Fernando Machado Martins"
+    },
+    {
+        "id": 13,
+        "disciplina": "Sociologia",
+        "sigla": false,
+        "mentor": "Ruth de Jesus Macedo"
+    }
+]
