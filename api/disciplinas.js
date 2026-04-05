91ac725abc5ce458eae39f691c4d658f825a4f9a
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2026-04-05 14:58:43 -0300

    feat: adicionar arrays

--- api/disciplinas.js
+++ api/disciplinas.js
@@ -5,61 +5,61 @@ export const DISCIPLINAS = [
         "id": 1,
         "disciplina": "Cargos, Carreira e Remuneração",
         "sigla": "CCR",
-        "mentor": false
+        "mentor": []
     },
     {
         "id": 2,
         "disciplina": "Comportamento Organizacional",
         "sigla": "CO",
-        "mentor": false
+        "mentor": ["Dharana Fabiana Pereira"]
     },
     {
         "id": 3,
         "disciplina": "Comunicação Corporativa",
         "sigla": "CC",
-        "mentor": false
+        "mentor": []
     },
     {
         "id": 4,
         "disciplina": "Ética e Cidadania Organizacional",
         "sigla": "ECO",
-        "mentor": "Marcos Batista dos Santos"
+        "mentor": ["Marcos Batista dos Santos"]
     },
     {
         "id": 5,
         "disciplina": "Gestão de Desempenho",
         "sigla": "GD",
-        "mentor": false
+        "mentor": []
     },
     {
         "id": 6,
         "disciplina": "Língua Espanhola",
         "sigla": "ESP",
-        "mentor": "Valdecir David da Silva"
+        "mentor": ["Valdecir David da Silva"]
     },
     {
         "id": 7,
         "disciplina": "Língua Inglesa",
         "sigla": "ING",
-        "mentor": false
+        "mentor": []
     },
     {
         "id": 8,
         "disciplina": "Língua Portuguesa",
         "sigla": "POR",
-        "mentor": "Abel José Mendes"
+        "mentor": ["Abel José Mendes"]
     },
     {
         "id": 9,
         "disciplina": "Matemática",
         "sigla": "MAT",
-        "mentor": "Eduardo Rodrigo Vedolin"
+        "mentor": ["Eduardo Rodrigo Vedolin"]
     },
     {
         "id": 10,
         "disciplina": "Modelos de Gestão",
         "sigla": "MG",
-        "mentor": false
+        "mentor": ["Dharana Fabiana Pereira"]
     },
     {
         "id": 11,
@@ -74,12 +74,12 @@ export const DISCIPLINAS = [
         "id": 12,
         "disciplina": "Qualidade de Vida, Saúde e Segurança do Trabalho",
         "sigla": "QVSST",
-        "mentor": "Luiz Fernando Machado Martins"
+        "mentor": ["Luiz Fernando Machado Martins"]
     },
     {
         "id": 13,
         "disciplina": "Sociologia",
         "sigla": "SOC",
-        "mentor": "Ruth de Jesus Macedo"
+        "mentor": ["Ruth de Jesus Macedo"]
     }
 ]
